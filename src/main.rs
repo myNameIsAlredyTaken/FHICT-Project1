b936944a468f2fe3427ecdbb03071848143529b7
myNameIsAlreadyTaken <bratygin.e@gmail.com>
2026-09-08 09:49:00 +0200

    minor changes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use bevy::prelude::*;
+use std::env;
 
 mod player;
 
 use player::TestPlugin;
 
 fn main() {
+    unsafe {
+        env::set_var("RUST_BACKTRACE", "1");
+    }
     App::new().add_plugins((DefaultPlugins, TestPlugin)).run();
 }
--- src/player.rs
+++ src/player.rs
@@ -2,13 +2,6 @@ use bevy::{ecs::query, prelude::*, state::commands};
 
 pub struct TestPlugin;
 
-impl Plugin for TestPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_systems(Startup, add_people);
-        app.add_systems(Update, (test, greet).chain());
-    }
-}
-
 #[derive(Component)]
 struct Name(String);
 
@@ -17,4 +10,21 @@ struct Person;
 
 fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Test".to_string())));
+}
+
+fn test() {
+    println!("123");
+}
+
+fn greet(query: Query<&Name, With<Person>>) {
+    for name in query {
+        println!("test {}", name.0);
+    }
+}
+
+impl Plugin for TestPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, add_people);
+        app.add_systems(Update, (test, greet).chain());
+    }
 }
